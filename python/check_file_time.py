b2b268f465c6576c88e4695f9b66e4760064e164
Munjiwon <wldnjsdl269@gmail.com>
2024-07-23 17:55:55 +0900

    class 형식으로 변경 및 시간 비교 함수 추가 현재 시간 시준 마지막 수정 시간과의 파이 계산하는 함수를 추가함 또한 타임스탬프형식의 데이터를 읽기쉽도록 변환하는 함수 추가

--- python/check_file_time.py
+++ python/check_file_time.py
@@ -1,19 +1,59 @@
 import os
-from datetime import datetime
+from datetime import datetime, timedelta
 
-#파일위치
-touch_file = os.path.expanduser("~/.system/touch.dat")
+class LastUseTime():
+    def __init__(self, filepath):
+        self.file = filepath
 
-def check_last_time():
-    '''
-    운영제체 별로 날짜/시간을 표현하는 방식이 다르며 유닉스와 리눅스는 1970-01-01 00:00:00부터 현재 시간까지의 초를 누적한 시간을 사용
-    이를 읽기 쉽도록 변환해줘야함
-    '''
-    time = os.path.getmtime(touch_file)
+    def nowTime(self):
+        # 현재 시스템은 utc기준
+        now = datetime.now().timestamp()
+        return now
 
-    time = datetime.fromtimestamp(time).strftime('%Y-%m-%d %H:%M:%S')
+    def lastTime(self):
+        last = os.path.getmtime(self.file)
+        return last
 
-    print(f"Last moditied time : {time}")
+    def checkTimestamp(self, time):
+        '''
+        운영제체 별로 날짜/시간을 표현하는 방식이 다르며 유닉스와 리눅스는 1970-01-01 00:00:00부터 현재 시간까지의 초를 누적한 시간을 사용
+        이를 읽기 쉽도록 변환해줘야함
+        '''
+        time = datetime.fromtimestamp(time).strftime('%Y-%m-%d %H:%M:%S')
+        print(f"convert time : {time}")
+        return time
+
+    def convertTime(self, time):
+        time = timedelta(seconds=time)
+        second = time.seconds
+        hour = second // 3600
+        second %= 3600
+        minute = second // 60
+        second %= 60
+        return hour, minute, second
+
+    def convertDay(self, time):
+        time = timedelta(seconds=time)
+        day = time.days
+        year = day // 365
+        day %= 365
+        month = day // 30
+        day %= 30
+        return year, month, day
+
+    def compareTime(self):
+        print("Compare....")
+        nowTime = self.nowTime()
+        lastTime = self.lastTime()
+        print(f"Time : {nowTime - lastTime}")
+        diffTime = nowTime - lastTime
+
+        year, month, day = self.convertDay(diffTime)
+        hour, minute, second = self.convertTime(diffTime)
+        print(f"Compare time : {year}-{month}-{day} {hour}:{minute}:{second}")
 
 if __name__ == "__main__":
-    check_last_time()
+    # 파일 경로
+    touch_file = os.path.expanduser("~/.system/touch.dat")
+    lt = LastUseTime(touch_file)
+    lt.compareTime()
