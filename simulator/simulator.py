741860d099a57dda2061dde07e4d405f9a5f4ff3
Jiwon Mun <wldnjsdl269@gmail.com>
2025-06-19 21:28:47 +0900

    [GEN685] 시뮬레이터의 메인 수행 함수 추가 - run 함수 추가   - 120초다 createPod함수 수행 - createPod함수 인자 추가   - 활성, 비활성, 믹스 각각 생성할 pod의 수를 인자로 받음 - 삭세 후 pod들이 완전히 종료되었음을 확인할 함수 추가(checkStatus)

--- simulator/simulator.py
+++ simulator/simulator.py
@@ -1,6 +1,5 @@
 from kubernetes import client, config, utils
-from os import path
-import yaml
+import time
 
 class Simulator:
     def __init__(self, namespace: str = 'gc-simulator'):
@@ -44,45 +43,56 @@ class Simulator:
         Run simulation
         """
         while self.count < self.times:
-            pass
+            cnt = 0
+            while cnt < 5:  # 120초마다 pod 생성
+                self.createPod(10, 6, 6)  # active 50, idle 30, mixed 30
+                time.sleep(self.intervalTime)
+                cnt += 1
 
+            # 실험 한 사이클 종료 후 pod 모두 삭제
+            self.deletePod()
+            while True:  # check delete pod status
+                if self.checkStatus():
+                    break
+                print("Deleting pod ------")
+                time.sleep(1)
+            self.count += 1
 
-    def createPod(self):
+    def createPod(self, ac, i, mx):
         """
         Create pod
         """
         cnt = 0
         #active pod
-        while cnt < 15:
+        while cnt < ac:
             self.pod_manifest['metadata']['name'] = 'experiment-active-'+str(cnt)
             next(env for env in self.pod_manifest['spec']['containers'][0]['env'] if env['name'] == 'MODE')['value'] = 'active'
             next(env for env in self.pod_manifest['spec']['containers'][0]['env'] if env['name'] == 'NUM_PROCS')['value'] = '3'
 
             self.coreV1.create_namespaced_pod(namespace=self.namespace, body=self.pod_manifest)
-            # utils.create_from_yaml(apiclient, dir_yaml, namespace=self.namespace, verbose=True)
-            print('pod',cnt,' created')
+            print('active pod', cnt, ' created')
             cnt += 1
 
         cnt = 0
         # idle pod
-        while cnt < 15:
+        while cnt < i:
             self.pod_manifest['metadata']['name'] = 'experiment-idle-' + str(cnt)
             next(env for env in self.pod_manifest['spec']['containers'][0]['env'] if env['name'] == 'MODE')['value'] = 'idle'
             next(env for env in self.pod_manifest['spec']['containers'][0]['env'] if env['name'] == 'NUM_PROCS')['value'] = '1'
 
             self.coreV1.create_namespaced_pod(namespace=self.namespace, body=self.pod_manifest)
-            print('pod', cnt, ' created')
+            print('idle pod', cnt, ' created')
             cnt += 1
 
         cnt = 0
         # mixed pod
-        while cnt < 15:
+        while cnt < mx:
             self.pod_manifest['metadata']['name'] = 'experiment-mixed-' + str(cnt)
             next(env for env in self.pod_manifest['spec']['containers'][0]['env'] if env['name'] == 'MODE')['value'] = 'mixed'
             next(env for env in self.pod_manifest['spec']['containers'][0]['env'] if env['name'] == 'NUM_PROCS')['value'] = '3'
 
             self.coreV1.create_namespaced_pod(namespace=self.namespace, body=self.pod_manifest)
-            print('pod', cnt, ' created')
+            print('mixed pod', cnt, ' created')
             cnt += 1
 
     def deletePod(self):
@@ -98,13 +108,31 @@ class Simulator:
         Get pod list in namespace
         """
         pods = self.coreV1.list_namespaced_pod(self.namespace).items
+        if not pods:
+            return
+
         for p in pods:
             p_name = p.metadata.name
             self.pod_list[p_name] = p
 
+    def checkStatus(self):
+        """
+        Pod가 모두 삭제되었는지 확인하는 함수
+        """
+        pods = self.coreV1.list_namespaced_pod(self.namespace).items
+        if not pods:  # No pod = clear to delete pods
+            print("Delete all pods")
+            return True
+        else:
+            return False
+
 if __name__ == "__main__":
     #네임스페이스 값을 비워두면 'default'로 지정
     gc = Simulator()
-    # gc.run()
+    gc.run()
     # gc.deletePod()
-    gc.createPod()
+    # while True:
+    #     if gc.checkStatus():
+    #         break
+    #     time.sleep(1)
+    # gc.createPod()
